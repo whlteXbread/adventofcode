ccadc0ddb91619830592f373050366bc1b04c679
Andrew Catellier <andrew@thisisreal.net>
2016-12-03 20:35:20 -0700

    another naive solution.

--- d2p1.py
+++ d2p1.py
@@ -1,8 +1,3 @@
-addends = {'U': -3,
-           'D': 3,
-           'L': -1,
-           'R': 1}
-
 lut = {1: {'U': 1, 'D': 4, 'L': 1, 'R': 2},
            2: {'U': 2, 'D': 5, 'L': 1, 'R': 3},
            3: {'U': 3, 'D': 6, 'L': 2, 'R': 3},
@@ -30,6 +25,33 @@ out_string = ''
 for line in inst_list:
     for character in line:
         current_value = lut[current_value][character]
+    out_string += str(current_value)
+
+print out_string
+
+new_lut = {1: {'U': 1, 'D': 3, 'L': 1, 'R': 1},
+           2: {'U': 2, 'D': 6, 'L': 2, 'R': 3},
+           3: {'U': 1, 'D': 7, 'L': 2, 'R': 4},
+           4: {'U': 4, 'D': 8, 'L': 3, 'R': 4},
+           5: {'U': 5, 'D': 5, 'L': 5, 'R': 6},
+           6: {'U': 2, 'D': 'A', 'L': 5, 'R': 7},
+           7: {'U': 3, 'D': 'B', 'L': 6, 'R': 8},
+           8: {'U': 4, 'D': 'C', 'L': 7, 'R': 9},
+           9: {'U': 9, 'D': 9, 'L': 8, 'R': 9},
+           'A': {'U': 6, 'D': 'A', 'L': 'A', 'R': 'B'},
+           'B': {'U': 7, 'D': 'D', 'L': 'A', 'R': 'C'},
+           'C': {'U': 8, 'D': 'C', 'L': 'B', 'R': 'C'},
+           'D': {'U': 'B', 'D': 'D', 'L': 'D', 'R': 'D'}}
+
+#inst_list = ['ULL',
+#             'RRDDD',
+#             'LURDL',
+#             'UUUUD']
+current_value = 5
+out_string = ''
+for line in inst_list:
+    for character in line:
+        current_value = new_lut[current_value][character]
     out_string += str(current_value)
 
 print out_string
